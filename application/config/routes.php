3a669abf5b51c0911a29ab7f636dad632afbb8a7
Bogdan C <tunetu_bog@yahoo.com>
2016-04-23 03:16:07 +0300

    Enviroment configurations

--- application/config/routes.php
+++ application/config/routes.php
@@ -41,6 +41,7 @@
 $route['default_controller'] = "welcome";
 $route['404_override'] = '';
 
+$route['login'] = "login_controller";
 
 /* End of file routes.php */
 /* Location: ./application/config/routes.php */
