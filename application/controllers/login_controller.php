a267ac7a807bbaa8f844bd754d9b50595a01c932
Bogdan C <tunetu_bog@yahoo.com>
2016-04-25 03:19:37 +0300

    Added form and validations for login module

--- application/controllers/login_controller.php
+++ application/controllers/login_controller.php
@@ -1,22 +1,42 @@
-<?php
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Login_controller extends Gameinfo_Controller
 {
-    const PAGE_TITLE_DESCRPTION = 'Login'; // TODO I need to make a lang for this
+    const PAGE_TITLE_DESCRIPTION = 'Login'; // TODO I need to make a lang for this
 
     private $cfg;
 
     public function __construct()
     {
         parent::__construct();
+
+        $this->load->helper(array('form', 'url'));
+        $this->load->library('form_validation');
     }
 
     public function index()
     {
         $this->cfg = array(
-            'title_description'   => self::PAGE_TITLE_DESCRPTION
+            'title_description'   => self::PAGE_TITLE_DESCRIPTION
         );
 
-        $this->loadTemplate('login', $this->cfg);
+        $this->validateForm();
+
+        if ($this->form_validation->run() == FALSE)
+        {
+            $this->loadTemplate('login', $this->cfg);
+        }
+        else
+        {
+            echo 'logged in';
+        }
+    }
+
+    private function validateForm()
+    {
+        $this->form_validation->set_rules('username', 'Username', 'trim|required|xss_clean');
+        $this->form_validation->set_rules('password', 'Password', 'trim|required|xss_clean');
+
+        $this->form_validation->set_error_delimiters('<div style="color: #E13300">', '</div>');
     }
 }
--- application/views/login.php
+++ application/views/login.php
@@ -1,1 +1,16 @@
-Login
+<?php echo form_open('login'); ?>
+<h3>Login</h3>
+
+<h5>Username</h5>
+<?php echo form_error('username'); ?>
+<input type="text" name="username" value="<?php echo set_value('username'); ?>" size="50" />
+
+<h5>Password</h5>
+<?php echo form_error('password'); ?>
+<input type="password" name="password" value="" size="50" />
+
+<br><br>
+
+<div><input type="submit" value="Submit" /></div>
+
+</form>
