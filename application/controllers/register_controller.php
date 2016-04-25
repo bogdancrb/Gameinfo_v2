cf3f759c7103a3c06ba197634b85a9f10eef1395
Bogdan C <tunetu_bog@yahoo.com>
2016-04-25 03:20:47 +0300

    Finished the register module

--- application/controllers/register_controller.php
+++ application/controllers/register_controller.php
@@ -1,22 +1,93 @@
-<?php
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Register_controller extends Gameinfo_Controller
 {
-    const PAGE_TITLE_DESCRPTION = 'Register'; // TODO I need to make a lang for this
+    const PAGE_TITLE_DESCRIPTION = 'Register'; // TODO I need to make a lang for this
 
     private $cfg;
 
     public function __construct()
     {
         parent::__construct();
+
+        $this->load->helper(array('form', 'url'));
+
+        $this->load->library('form_validation');
+
+        $this->load->model('register_model');
     }
 
     public function index()
     {
         $this->cfg = array(
-            'title_description'   => self::PAGE_TITLE_DESCRPTION
+            'title_description'   => self::PAGE_TITLE_DESCRIPTION
         );
 
-        $this->loadTemplate('register', $this->cfg);
+        $this->validateForm();
+
+        if ($this->form_validation->run() == FALSE)
+        {
+            $this->loadTemplate('register', $this->cfg);
+        }
+        else
+        {
+            $post = $this->input->post('register');
+
+            $message = $this->register_model->addNewUser($post);
+
+            echo $message;
+        }
+    }
+
+    private function validateForm()
+    {
+        $this->form_validation->set_rules('register[username]', 'Username', 'trim|required|min_length[4]|max_length[15]|callback_username_check|xss_clean');
+        $this->form_validation->set_rules('register[password]', 'Password', 'trim|required|min_length[6]|callback_password_check|xss_clean');
+        //$this->form_validation->set_rules('register[passconf]', 'Password Confirmation', 'trim|required|matches[register[password]]|xss_clean'); // TODO Investigate why matches[] doesn't work with array
+        $this->form_validation->set_rules('register[email]', 'Email', 'trim|required|valid_email|callback_email_check|xss_clean');
+        $this->form_validation->set_rules('register[country]', 'Country', 'required|xss_clean');
+
+        $this->form_validation->set_error_delimiters('<div style="color: #E13300">', '</div>');
+    }
+
+    public function username_check($username)
+    {
+        $error = $this->register_model->checkUsername($username);
+
+        if ($error)
+        {
+            $this->form_validation->set_message('username_check',
+                'The Username is already taken, please chose another one.'); // TODO I need to make a lang for this
+            return false;
+        }
+        
+        return true;
+    }
+
+    public function email_check($email)
+    {
+        $error = $this->register_model->checkEmailAddress($email);
+
+        if ($error)
+        {
+            $this->form_validation->set_message('email_check',
+                'The Email is already taken, did you <a href="#">forget the password</a> ?'); // TODO I need to make a lang for this
+            return false;
+        }
+
+        return true;
+    }
+
+    public function password_check($password)
+    {
+        // check if the password has at least an uppercase and a number included
+        if ( !(preg_match('~[A-Z]~', $password) && preg_match('~[a-z]~', $password) && preg_match('~\d~', $password)) )
+        {
+            $this->form_validation->set_message('password_check',
+                    'The Password field does not contain at least a number and an uppercase letter.'); // TODO I need to make a lang for this
+            return false;
+        }
+
+        return true;
     }
 }
--- application/views/register.php
+++ application/views/register.php
@@ -1,1 +1,28 @@
-Register
+<?php echo form_open('register'); ?>
+<h3>Register</h3>
+
+<h5>Username</h5>
+<?php echo form_error('register[username]'); ?>
+<input type="text" name="register[username]" value="<?php echo set_value('register[username]'); ?>" size="50" />
+
+<h5>Password</h5>
+<?php echo form_error('register[password]'); ?>
+<input type="password" name="register[password]" value="" size="50" />
+
+<h5>Password Confirm</h5>
+<?php echo form_error('register[passconf]'); ?>
+<input type="password" name="register[passconf]" value="" size="50" placeholder="SKIP THIS" readonly="readonly"/>
+
+<h5>Email Address</h5>
+<?php echo form_error('register[email]'); ?>
+<input type="text" name="register[email]" value="<?php echo set_value('register[email]'); ?>" size="50" />
+
+<h5>Country</h5>
+<?php echo form_error('register[country]'); ?>
+<input type="text" name="register[country]" value="<?php echo set_value('register[country]'); ?>" size="50" />
+
+<br><br>
+
+<div><input type="submit" value="Submit" /></div>
+
+</form>
