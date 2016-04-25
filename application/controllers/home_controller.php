dcde3220d71547699eda0907049e55460063918d
Bogdan C <tunetu_bog@yahoo.com>
2016-04-25 03:17:17 +0300

    Fixed wrong typo for description variable

--- application/controllers/home_controller.php
+++ application/controllers/home_controller.php
@@ -1,8 +1,8 @@
-<?php
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Home_controller extends Gameinfo_Controller
 {
-    const PAGE_TITLE_DESCRPTION = 'Home'; // TODO I need to make a lang for this
+    const PAGE_TITLE_DESCRIPTION = 'Home'; // TODO I need to make a lang for this
 
     private $cfg;
 
@@ -14,7 +14,7 @@ class Home_controller extends Gameinfo_Controller
     public function index()
     {
         $this->cfg = array(
-            'title_description'   => self::PAGE_TITLE_DESCRPTION
+            'title_description'   => self::PAGE_TITLE_DESCRIPTION
         );
 
         $this->loadTemplate('home', $this->cfg);
