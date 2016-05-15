c278c36ef17010a6b817c938cb263f98ae5b8a59
Bogdan C <tunetu_bog@yahoo.com>
2016-05-16 01:07:38 +0300

    Added a TODO

--- application/controllers/logout_controller.php
+++ application/controllers/logout_controller.php
@@ -21,11 +21,11 @@ class Logout_controller extends Gameinfo_Controller
         {
             clear_session_data();
             
-            $this->data['message'] = 'You have logged out of your account. <br> Redirecting to home page in 5 seconds.';
+            $this->data['message'] = 'You have logged out of your account. <br> Redirecting to home page in 5 seconds.'; // TODO I need to make a lang for this
         }
         else
         {
-            $this->data['message'] = 'You are not logged in !';
+            $this->data['message'] = 'You are not logged in !'; // TODO I need to make a lang for this
         }
 
         $this->loadTemplate('logout', $this->data);
