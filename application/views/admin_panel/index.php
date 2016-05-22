3e154f0cd4fbf0c49c3277c533e1e251097d3d4d
Bogdan C <tunetu_bog@yahoo.com>
2016-05-22 22:39:31 +0300

    Added some admin panel functionality

---
+++ application/views/admin_panel/index.php
@@ -0,0 +1,15 @@
+<div id="container">
+    <h1><?php echo $page_name; ?></h1>
+
+    <!-- TODO Make a lang file for the whole page -->
+
+    <div id="body">
+        <?php echo $menu; ?>
+        <p>
+            Welcome to the Administrator Panel.<br>
+            Here you can manage the news articles, users and other site related settings.<br>
+            <br>
+            <b>{HERE WILL BE ADDED SOME STATISTICS IN THE FUTURE RELEASES}</b>
+        </p>
+    </div>
+</div>
